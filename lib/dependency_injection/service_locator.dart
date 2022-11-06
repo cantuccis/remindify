8badbf0fe19bd97ca9c40ba226e35e3f85012a79
Facundo Arancet Iza <facuarancet97@gmail.com>
2022-11-06 19:56:39 -0300

    Refactor notification services to avoid coupling to AwesomeNotifications package

--- lib/dependency_injection/service_locator.dart
+++ lib/dependency_injection/service_locator.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/foundation.dart';
 import 'package:get_it/get_it.dart';
 import 'package:remindify/firebase/repositories/firebase_reminder_repository.dart';
 import 'package:remindify/firebase/repositories/firebase_user_repository.dart';
@@ -5,6 +6,8 @@ import 'package:remindify/firebase/services/firebase_auth_service.dart';
 import 'package:remindify/interfaces/repositories/reminder_repository.dart';
 import 'package:remindify/interfaces/repositories/user_repository.dart';
 import 'package:remindify/interfaces/services/auth_service.dart';
+import 'package:remindify/interfaces/services/notifications_service.dart';
+import 'package:remindify/services/awesome_notifications_service.dart';
 
 class ServiceLocator {
   static final ServiceLocator instance = ServiceLocator._internal();
@@ -19,11 +22,13 @@ class ServiceLocator {
     final userRepository = FirebaseUserRepository();
     final auth = FirebaseAuthService(userRepository: userRepository);
     final reminderRepo = FirebaseReminderRepository();
+    final notificationsService = AwesomeNotificationsService()..initialize();
 
     getIt
       ..registerSingleton<AuthService>(auth)
       ..registerSingleton<UserRepository>(userRepository)
-      ..registerSingleton<ReminderRepository>(reminderRepo);
+      ..registerSingleton<ReminderRepository>(reminderRepo)
+      ..registerSingleton<NotificationsService>(notificationsService);
   }
 
   ServiceLocator._internal();
